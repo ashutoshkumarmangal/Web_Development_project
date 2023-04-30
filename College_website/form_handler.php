30e3ca93cdbb953854654f452a479b8837365b1c
Ashutosh Kumar Mangal <92458131+ashutoshkumarmangal@users.noreply.github.com>
2023-04-30 10:32:40 +0530

    Add files via upload

--- College_website/form_handler.php
+++ College_website/form_handler.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from ='aashuakm12@gmail.com';
+$email_from ='https://tit-bhiwani.netlify.app/';//basically you put the site domain name here and the site sends the mail.
 
 $email_subject='New Form Submission';
 
@@ -18,11 +18,11 @@ $to = 'aashuakm12@gmail.com';
 
 $headers="From: $email_from\r\n";
 
-$headers.="Reply-To:
+$headers .="Reply-To:
 $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location:contact.html");
+header("Location: contact.html");
 
 ?>
