890ef78380c310b1f9cf6a561f0fb7150c09b673
Ashutosh Kumar Mangal <92458131+ashutoshkumarmangal@users.noreply.github.com>
2023-08-20 23:07:04 +0530

    Add files via upload

---
+++ QR code generator/script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;//if the input is empty return from here
+
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    //getting a QR code of user entered value using the qrserver api and passing the api returned img src to qrImg
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
