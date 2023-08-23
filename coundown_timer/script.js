8feeb3c48ce1c0496e0fa4b75331186c7e59d42b
Ashutosh Kumar Mangal <92458131+ashutoshkumarmangal@users.noreply.github.com>
2023-08-23 14:56:55 +0530

    Add files via upload

--- coundown_timer/script.js
+++ coundown_timer/script.js
@@ -3,7 +3,7 @@ const hoursE1 = document.getElementById('hours');
 const minsE1 = document.getElementById('mins');
 const secondsE1 = document.getElementById('seconds');
 
-const championsLeague = '11 Jun 2023';
+const championsLeague = '11 Jun 2026';
 
 function countdown() {
     const championsLeagueDate = new Date(championsLeague);
@@ -17,15 +17,18 @@ function countdown() {
     const seconds = Math.floor(totalseconds) % 60;
 
     daysE1.innerHTML = days;
-    hoursE1.innerHTML = formatTime(hours);
+    hoursE1.innerHTML = formatTime(hours); 
+
     minsE1.innerHTML = formatTime(mins);
     secondsE1.innerHTML = formatTime(seconds);
 
     function formatTime(time){
         return time < 10 ? (`0${time}` ) : time;
     }
+    //  The formatTime function is used to add a leading zero to the time if it is less than 10.
 }
 
 countdown();
 
 setInterval(countdown, 1000); //here 1000ms = 1 sec
+// after every 1 sec the countdown function is called
